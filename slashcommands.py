0107641908d0f6e1f4418f6b8b632aac840a8f0b
Melumi <roy.zixiao.huang@gmail.com>
2021-05-01 16:53:39 -0400

    Update slashcommands.py

--- slashcommands.py
+++ slashcommands.py
@@ -6,25 +6,24 @@ from discord_slash.utils.manage_commands import create_option
 def init_slashcommands(slash):
     print("slash commands initialized!")
 
+    init_download(slash)
+
     # Roll
     @slash.slash(name="roll", description="Dice roll command, up to 999,999,999",
                 options=[create_option(
                     name="d",
                     description="Roll a d-sided die",
-                    option_type=3,
+                    option_type=4,
                     required=True)])
     async def roll(ctx, d):
-        if len(d) < 17:  # dice roll command (up to 999,999,999)
-            try:
-                await ctx.send(content=f"Roll d{d}: `[{str(random.randint(1, int(d)))}]`")
-            except:
-                await ctx.send(content="Please enter a number between zero and one billion")
+        if 0 < d <= 999999999:  # dice roll command (up to 999,999,999)
+            await ctx.send(content=f"Roll d{d}: `[{str(random.randint(1, d))}]`")
+        else:
+            await ctx.send(content="Please enter a number between one and one billion")
 
 
-    def my_hook(d):
-        if d['status'] == 'finished':
-            print('Done downloading, now converting ...')
-
+# initialize specific shash commands
+def init_download(slash):
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -32,7 +31,7 @@ def init_slashcommands(slash):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'progress_hooks': [my_hook]}
+        'progress_hooks': []}
 
     @slash.slash(name="download",
                 description="Find an audio file, given a link. Use this if you want to quickly download something.",
@@ -41,10 +40,11 @@ def init_slashcommands(slash):
                     description="Link to the media you want to find.",
                     option_type=3,
                     required=True)])
-    async def download(ctx, Source):
+    async def download(ctx, source):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             try:
-                r = ydl.extract_info(Source, download=False)
+                r = ydl.extract_info(source, download=False)
                 await ctx.send(r['url'])
             except Exception as exception:
-                await ctx.send(f"**{type(exception).__name__}**: `{str(exception)[18:-89]}`")
+                length = 19 + len(source) + 21
+                await ctx.send(f"**{type(exception).__name__}**: `{str(exception)[18:length]}`")
