3ca9200fa5c38418b2f3a2f5dcb79b5d581b6a5d
Melumi <roy.zixiao.huang@gmail.com>
2021-05-01 17:32:46 -0400

    Update slashcommands.py
    create separate functions

--- slashcommands.py
+++ slashcommands.py
@@ -3,27 +3,23 @@ import random
 # from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option
 
-def init_slashcommands(slash):
-    print("slash commands initialized!")
 
-    init_download(slash)
-
-    # Roll
-    @slash.slash(name="roll", description="Dice roll command, up to 999,999,999",
-                options=[create_option(
+def roll(slash):
+    description = "Dice roll command, up to 999,999,999"
+    options = [create_option(
                     name="d",
                     description="Roll a d-sided die",
                     option_type=4,
-                    required=True)])
+                    required=True)]
+    
+    @slash.slash(name="roll", description=description, options=options)
     async def roll(ctx, d):
         if 0 < d <= 999999999:  # dice roll command (up to 999,999,999)
             await ctx.send(content=f"Roll d{d}: `[{str(random.randint(1, d))}]`")
         else:
             await ctx.send(content="Please enter a number between one and one billion")
 
-
-# initialize specific shash commands
-def init_download(slash):
+def download(slash):
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -33,13 +29,16 @@ def init_download(slash):
         }],
         'progress_hooks': []}
 
-    @slash.slash(name="download",
-                description="Find an audio file, given a link. Use this if you want to quickly download something.",
-                options=[create_option(
+    description = "Find an audio file, given a link. Use this if you want to quickly download something."
+    options=[create_option(
                     name="Source",
                     description="Link to the media you want to find.",
                     option_type=3,
-                    required=True)])
+                    required=True)]
+
+    @slash.slash(name="download",
+                description=description,
+                options=options)
     async def download(ctx, source):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             try:
@@ -48,3 +47,11 @@ def init_download(slash):
             except Exception as exception:
                 length = 19 + len(source) + 21
                 await ctx.send(f"**{type(exception).__name__}**: `{str(exception)[18:length]}`")
+
+
+commands = {roll, download}
+
+def init_slashcommands(slash):
+    for i in commands:
+        i(slash)
+    print("slash commands initialized!")
