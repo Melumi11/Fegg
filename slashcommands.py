9d9f52fbb01535205a8dec2533cf73006a33c11f
Melumi <roy.zixiao.huang@gmail.com>
2021-04-30 22:48:46 -0400

    Replace many files with one file

---
+++ slashcommands.py
@@ -0,0 +1,54 @@
+from Fegg import slash
+import youtube_dl
+import random
+from discord_slash.utils.manage_commands import create_option
+
+
+# Sweat
+@slash.slash(name="sweat", description=":colinsweat:")
+async def sweat(ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/822493563619246131/822498710873178133/unknown.png")
+
+
+# Roll
+@slash.slash(name="roll", description="Dice roll command, up to 999,999,999",
+            options=[create_option(
+                name="d",
+                description="Roll a d-sided die",
+                option_type=3,
+                required=True)])
+async def roll(ctx, d):
+    if len(d) < 17:  # dice roll command (up to 999,999,999)
+        try:
+            await ctx.send(content=f"Roll d{d}: `[{str(random.randint(1, int(d)))}]`")
+        except:
+            await ctx.send(content="Please enter a number between zero and one billion")
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'progress_hooks': [my_hook]}
+
+@slash.slash(name="download",
+            description="Find an audio file, given a link. Use this if you want to quickly download something.",
+            options=[create_option(
+                name="Source",
+                description="Link to the media you want to find.",
+                option_type=3,
+                required=True)])
+async def download(ctx, Source):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            r = ydl.extract_info(Source, download=False)
+            await ctx.send(r['url'])
+        except Exception as exception:
+            await ctx.send(f"**{type(exception).__name__}**: `{str(exception)[18:-89]}`")
--- slashcommands/sweat.py
+++
@@ -1,5 +0,0 @@
-# Sweat
-def init(slash):
-    @slash.slash(name="sweat", description=":colinsweat:")
-    async def sweat(ctx):
-        await ctx.send("https://cdn.discordapp.com/attachments/822493563619246131/822498710873178133/unknown.png")
